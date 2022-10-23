e8f52b8a425281fad58022161714b9cebc09b185
Elvin Mammadli <mamedli.elvin@gmail.com>
2022-10-23 23:24:16 +0400

    order component & cart created

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -50,7 +50,21 @@ const Cart = () => {
             </tr>
           </tbody>
         </table>
-        <div className={styles.right}></div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount:</b>$0.00
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>$79.60
+          </div>
+          <button className={styles.button}>Checkout NOW!</button>
+        </div>
       </div>
     </div>
   )
---
+++ pages/orders/[id].jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { useState } from "react";
+import styles from "../styles/Order.module.css";
+
+const Order = () => {
+  return (
+    <div>Order</div>
+  )
+}
+
+export default Order
